8d12b83c0d111cdbbe36ae7d56e0ea6f0b58d1ce
Dan Moore <mooreds@transposit.com>
2019-08-26 20:13:12 +0000

    different way of parsing out the email address

--- src/webhook.js
+++ src/webhook.js
@@ -6,34 +6,41 @@
  * https://www.transposit.com/docs/building/webhooks
  */
 ({ http_event }) => {
-  const parsed_body = http_event.parsed_body;
-  const workspaceId = parsed_body.team_id;
-  const userId = parsed_body.user_id;
-  const response_url = parsed_body.response_url;
+  const parsed_body = JSON.parse(http_event.body)
+  const command = parsed_body.text
+
+  const parsed_body.text
 
   setImmediate(() => {
-    let user = api.user({type: "slack", workspaceId, userId});
-    if (user) {
-      console.log(user)
-      var text_match = /(\S+) (\S+)/.exec(parsed_body.text.trim());
-      if (text_match) {
-		let message = api.run('this.transfer_file', { source_url: text_match[1], target_url: text_match[2] }, {asUser: user.id})[0];
-        api.run("slack_webhook.post_to_response_url", {
-          response_url: response_url,
-          post_body: {text: message}
-        });      
+    const text_match = /(\S+) (\S+) (\S+) (\S+)/.exec(parsed_body.text.trim());
+    const userId = text_match[3];
+    const source_url = text_match[1];
+    const target_url = text_match[2];
+    if (userId) {
+ 	  
+      let user = api.user({type: "google", email: userId});  
+      if (user) {
+		text = api.run('this.transfer_file', { source_url: source_url, target_url: target_url }, {asUser: user.id})[0];
       } else {
-        api.run("slack_webhook.post_to_response_url", {
-          response_url: response_url,
-          post_body: {text: "Couldn't parse the source and target urls."}
-        });      
+        text = 'Couldn\'t parse the source and target urls.';    
       }
     } else {
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: {text: 'Please configure your user at ' +  env.getBuiltin().appUrl}
-      });      
+      text = 'Please configure your user at ' +  env.getBuiltin().appUrl;   
     }
   });
-  return { status_code: 200 };
+  
+  const body = {
+    "type": "message",
+    "text": text
+  }
+  return { status_code: 200, body: body };
 }
+
+
+/**
+ * This operation is an example of a JavaScript operation deployed as a Webhook
+ * and configured to work with Slack.
+ *
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/building/webhooks
+ */
