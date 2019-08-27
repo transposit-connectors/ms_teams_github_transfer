ae04a90d5d5a769bf65a4138d159939d95ecba7a
Dan Moore <mooreds@transposit.com>
2019-08-27 17:32:19 +0000

    corrected error message

--- src/getErrorMessage.js
+++ src/getErrorMessage.js
@@ -4,7 +4,7 @@
         if (bot_name === null) {
             bot_name = "Bot";
         }
-        return 'Usage: ' + bot_name + ' github-source-file-url github-destination-file-url\n\nAlso, please configure your user at <a href="' + setupUrl + '">' + setupUrl + '</a> and then run "configure <email address>"';
+        return 'Usage: ' + bot_name + ' github-source-file-url github-destination-file-url\n\nAlso, please configure your user at <a href="' + setupUrl + '">' + setupUrl + '</a> and then run "configure transposit-email-address"';
     }
 
 /*
