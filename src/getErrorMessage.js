44647fa7e32ba503e6c5d37103ffe98e8ea541bb
Dan Moore <mooreds@transposit.com>
2019-09-26 19:22:35 +0000

    made message better

--- src/getErrorMessage.js
+++ src/getErrorMessage.js
@@ -1,10 +1,5 @@
 (params) => {
         const setupUrl = env.getBuiltin().appUrl;
         const bot_name = params.bot_name;
-        return 'Usage: ' + bot_name + ' github-source-file-url github-destination-file-url\n\nBut first, please both configure your user at <a href="' + setupUrl + '">' + setupUrl + '</a> and also type, in Teams, "@'+bot_name+' configure transposit-email-address"';
+        return 'Usage: @' + bot_name + ' github-source-file-url github-destination-file-url\n\nBut first, please both configure your user at <a href="' + setupUrl + '">' + setupUrl + '</a> and also type, in Teams, "@'+bot_name+' configure [email-address-you-signed-up-with-in-transposit]"';
     }
-
-/*
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/references/js-operations
- */
